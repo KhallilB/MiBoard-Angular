828a3acabc5d7c5dadf64776cb025319c969b784
KhallilB <khallil.bailey@gmail.com>
2019-01-10 23:50:16 -0800

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
+//Modules
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
+//Components
 import { AppComponent } from "./app.component";
+import { CommunityComponent } from "./community/community.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home/home.component";
+
+//Routes
+const appRoutes: Routes = [
+  { path: "community", component: CommunityComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "", component: HomeComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    CommunityComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
