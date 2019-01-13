2ebbbb6cbffb3874c28413dcb0ac6d18c8decf9d
KhallilB <khallil.bailey@gmail.com>
2019-01-12 18:45:42 -0800

    creating http request

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from "@angular/core";
 import { User } from "./user.model";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   selectedUser: User = {
     fullName: "",
     email: "",
     password: ""
   };
+
+  postUser(user: User) {
+    return this.http.post(environment.apiBaseUrl + "/signup", user);
+  }
 }
