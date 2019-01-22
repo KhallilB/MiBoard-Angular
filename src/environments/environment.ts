5e371409928dc8f96d012e2990e3df29c83bb7be
KhallilB <khallil.bailey@gmail.com>
2019-01-22 12:03:48 -0800

    api urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiBaseUrl: "http://bit.ly/miboardservices"
+  production: false,
+  apiBaseUrl: "http://localhost:3500/api"
 };
 
 /*
